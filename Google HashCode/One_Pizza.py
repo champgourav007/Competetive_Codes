dc0c635a66304e8a44d106219f10271cb2d31675
champgourav007 <gourav.19b131001@abes.ac.in>
2022-02-23 17:48:46 +0530

    Added Google HashCode Example Problem and score is 1840

---
+++ Google HashCode/One_Pizza.py
@@ -0,0 +1,19 @@
+t = int(input())
+likes = []
+dislikes = []
+for _ in range(t):
+    l = input().split()
+    d = input().split()
+    n1 = int(l[0])
+    for i in l[1:]:
+        likes.append(i)
+    n2 = int(d[0])
+    for i in d[1:]:
+        dislikes.append(i)
+# print(likes)
+for i in dislikes:
+    if len(i) != 0:
+        count = likes.count(i)
+        for j in range(count):
+            likes.remove(i)
+print(len(set(likes)), *set(likes))
